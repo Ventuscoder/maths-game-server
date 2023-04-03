a9a5c0e77eab0cc0c993b68341bae7207ae4aa79
ventuscoder <shivankcode@gmail.com>
2023-04-03 08:31:14 +0530

    Setup project and make basic routes

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.render()
+})
+
+app.get('/register', (req, res) => {
+    res.render()
+})
+
+app.post('/register', (req, res) => {
+    
+})
+
+app.get('/login', (req, res) => {
+    res.render()
+})
+
+app.post('/login', (req, res) => {
+    
+})
+
+app.get('/logout', (req, res) => {
+    
+})
